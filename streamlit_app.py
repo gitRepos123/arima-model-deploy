8d1f0730c3080beb6afa8e792915955885640085
Samarth Chaurasia <104571426+gitRepos123@users.noreply.github.com>
2024-04-01 07:42:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.header('Electrical Production Based ARIMA Model')
 def make_response(days: int = 1) -> None:
     days_array = [ x+1 for x in range(days) ]
     model = joblib.load('trained_model.pk1')
-    predictions = model.forcast(start = 1, end = days)
+    predictions = model.predict(start = 1, end = days)
     data = pd.DataFrame({
         'Days': days_array,
         'Predictions': predictions
